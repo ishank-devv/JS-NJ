d83eb8de6d176ebde92d1923ad263acffafeda1b
Ishank Suri <ishanksuri01@gmail.com>
2025-03-04 14:27:24 +0530

    Starting Self-practice-2025

---
+++ Self-practice-2025/1-closure/1privateCounter.js
@@ -0,0 +1,19 @@
+function createCounter() {
+  // Your code here
+  let count = 0;
+  return {
+    increment: function () {
+      count++;
+      console.log(count);
+    },
+    decrement: function () {
+      count--;
+      console.log(count);
+    },
+  };
+}
+
+const counter = createCounter();
+counter.increment(); // 1
+counter.increment(); // 2
+counter.decrement(); // 1
